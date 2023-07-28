8865182ce9670879f7411b274fe8e35f997b425d
SurvivrrHayde <wsd6vn@virginia.edu>
2023-07-28 16:18:30 -0400

    express setup

---
+++ backend/helpers/uva-sis-urls.ts
@@ -0,0 +1,3 @@
+export const sisUrls = {
+    csClassesFall2023: "https://sisuva.admin.virginia.edu/psc/ihprd/UVSS/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula.IScript_ClassSearch?institution=UVA01&term=1238&subject=CS"
+}
