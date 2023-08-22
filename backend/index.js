5f65f19c2722e7efeeb0a523fb4b82e45289db69
Matthew Haid <wsd6vn@virginia.edu>
2023-08-21 22:37:21 -0400

    message

--- backend/index.js
+++ backend/index.js
@@ -5,20 +5,22 @@ const path = require("path");
 const app = express();
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, "client/build")));
 
-app.get('/api/csClassesFall2023', async(req, res) => {
-  const response = await axios.get("https://sisuva.admin.virginia.edu/psc/ihprd/UVSS/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula.IScript_ClassSearch?institution=UVA01&term=1238&subject=CS");
+app.get("/api", async (req, res) => {
+  const response = await axios.get(
+    "https://sisuva.admin.virginia.edu/psc/ihprd/UVSS/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula.IScript_ClassSearch?institution=UVA01",
+  );
   res.json(response);
 });
 
 // catchall handler
-app.get('*', (req, res) => {
-  const Error = "Error: No URL matched any api calls"
+app.get("*", (req, res) => {
+  const Error = "Error: No URL matched any api calls";
   res.json(Error);
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 app.listen(port);
 
-console.log("Successfully ran on port 5000.");
+console.log("Successfully ran on port 8000.");
